5058d4bb4e4787006d9ec48ed50a11dd4ebdc64d
cherryklkulshrestha <240929815+cherryklkulshrestha@users.noreply.github.com>
2025-11-15 02:31:47 +0530

    Update CORS to include new Vercel frontend

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5001;
 // =========================
 const allowedOrigins = [
   'http://localhost:5173',
- 'https://sai-naman-frontend-c4db.vercel.app',
+ 'https://sai-naman-frontend.vercel.app',
  'https://sai-naman-frontend-c4db-2ebmfzyhk-sai-namans-projects.vercel.app'
 ];
 
