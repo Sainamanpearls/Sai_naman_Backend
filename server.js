c175b8c5c5cbffc5228fbd2efccca4c13da9aac0
cherryklkulshrestha <240929815+cherryklkulshrestha@users.noreply.github.com>
2025-11-15 21:36:01 +0530

    new connection

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ const PORT = process.env.PORT || 5001;
 // =========================
 const allowedOrigins = [
   'http://localhost:5173',
- 'https://sai-naman-frontend.vercel.app',
- 'https://sai-naman-frontend-c4db-2ebmfzyhk-sai-namans-projects.vercel.app'
+  'https://www.sainamanpearls.com/'
 ];
 
 app.use(
